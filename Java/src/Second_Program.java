186eb8a3caa49ec877fcac85c0533dc8bcbc112b
pranamyabhatt <pranamyabhatt@LAPTOP-V9RJ7H0E>
2020-06-23 21:05:15 +0530

    Added Second_programm Class

---
+++ Java/src/Second_Program.java
@@ -0,0 +1,9 @@
+
+public class Second_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Java/src/first.java
+++ Java/src/first.java
@@ -2,8 +2,7 @@
 public class first {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Chnages");
 	}
 
 }
